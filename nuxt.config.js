9ea3996c83d9bac5c271e4c8f539c466d4ca215f
Amol derick soans <amol.optimus@gmail.com>
2021-07-26 20:25:34 +0530

    configured i18n and added folders for spanish , french and japanese

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,4 +13,29 @@ export default theme({
     // Doc: https://github.com/nuxt-community/tailwindcss-module
     "@nuxtjs/tailwindcss",
   ],
+
+  i18n: {
+    locales: () => [{
+      code: 'es',
+      iso: 'es-ES',
+      file: 'es-ES.js',
+      name: 'Español'
+    }, {
+      code: 'fr',
+      iso: 'fr-FR',
+      file: 'fr-FR.js',
+      name: 'Français'
+    }, {
+      code: 'ja',
+      iso: 'ja-JP',
+      file: 'ja-JP.js',
+      name: '日本語'
+    }, {
+      code: 'en',
+      iso: 'en-US',
+      file: 'en-US.js',
+      name: 'English'
+    }],
+    defaultLocale: 'en'
+  },
 });
