eeba06d190fa28adf946439ddf5fa464290766da
liyasthomas <liyascthomas@gmail.com>
2021-11-17 16:15:19 +0530

    chore(deps): bump

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,18 +5,5 @@
  ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
  */
 module.exports = {
-  theme: {
-    extend: {
-      typography: {
-        DEFAULT: {
-          css: {
-            img: {
-              margin: "0 !important",
-            },
-          },
-        },
-      },
-    },
-  },
   plugins: [require("@tailwindcss/typography")],
 };
