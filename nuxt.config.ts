a7af76e4fad68d06e3991453348ad70b4e3a5d11
Liyas Thomas <liyascthomas@gmail.com>
2023-04-14 14:02:48 +0530

    fix: algolia docsearch

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,7 @@ export default defineNuxtConfig({
       algolia: {
         apiKey: process.env.ALGOLIA_SEARCH_API_KEY,
         applicationId: process.env.ALGOLIA_APPLICATION_ID,
+        langAttribute: "lang",
         docSearch: {
           indexName: process.env.ALGOLIA_INDEX_NAME,
         },
