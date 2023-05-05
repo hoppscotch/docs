8b384ec532f9f42d161a8d3649a5a76caeb58b0d
Liyas Thomas <liyascthomas@gmail.com>
2023-05-05 17:05:37 +0530

    chore: updated screenshots with color mode support

---
+++ components/content/Image.vue
@@ -0,0 +1,32 @@
+<script setup>
+defineProps({
+  src: {
+    type: String,
+    required: true,
+  },
+  alt: {
+    type: String,
+    required: true,
+  },
+  extension: {
+    type: String,
+    required: true,
+  },
+})
+
+const colorMode = useColorMode()
+</script>
+
+<template>
+  <p style="margin: var(--prose-li-margin);">
+    <img
+      v-if="!colorMode.unknown"
+      :src="`${src}-${colorMode.value}.${extension}`"
+      :alt="alt"
+      style="
+      border: 1px solid var(--elements-border-primary-static);
+      border-radius: var(--radii-md);
+    "
+    >
+  </p>
+</template>
--- components/content/Logo.vue
+++ components/content/Logo.vue
@@ -1,20 +1,13 @@
 <template>
-  <div class="row">
-    <img src="/logo.svg" class="logo" alt="Hoppscotch">
-  </div>
+  <img src="/logo.svg" class="logo" alt="Hoppscotch">
 </template>
 
 <style scoped lang="ts">
 css({
-  '.row':{
-    display: 'flex',
-    alignItems: 'center',
-  },
   '.logo': {
     borderRadius: '0.25rem',
     width: '2rem',
     height: '2rem',
-    marginRight: '1rem',
   }
 })
 </style>
