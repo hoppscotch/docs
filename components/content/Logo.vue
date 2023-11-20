ee1d89709e3e335d1f1751924eff690cff727a6e
Liyas Thomas <liyascthomas@gmail.com>
2023-11-20 12:44:00 +0530

    chore: updates

--- components/content/Logo.vue
+++ components/content/Logo.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex items-center">
-    <img src="/logo.svg" class="!h-8 !w-8 rounded" alt="Hoppscotch" />
+    <img src="/logo.svg" class="!h-8 !w-8 rounded" alt="Hoppscotch">
     <!-- <span class="ml-2 text-lg font-bold sm:hidden lg:flex"> Hoppscotch </span> -->
   </div>
 </template>
