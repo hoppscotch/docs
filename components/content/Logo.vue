2de504b6db73703dba1badce3dc72a04258e510a
Liyas Thomas <liyascthomas@gmail.com>
2024-01-06 21:21:13 +0530

    chore: remove docus

--- components/content/Logo.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="flex items-center">
-    <img src="/logo.svg" class="!h-8 !w-8 rounded" alt="Hoppscotch">
-    <!-- <span class="ml-2 text-lg font-bold sm:hidden lg:flex"> Hoppscotch </span> -->
-  </div>
-</template>
